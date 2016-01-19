42bc1fb37bffaef1ee7f8ee712451f894ad4b0eb
Rey Marin Jr <rey.marin@gmail.com>
2016-01-18 23:56:48 -0800

    Code formatting: converted tabs to spaces

--- js/content.js
+++ js/content.js
@@ -1,231 +1,231 @@
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-	var data;
+    var data;
 
-	if (request.action === "download") {
-		download(request.data);
-	} else if (request.action === "make_list") {
-		data = scrape(request.data);
-		download(data);
-		sendResponse({data: data});
-	} else if (request.action === 'next_page') {
-		window.location.href = $('#right .pagination a:last').attr('href');
-	} else if (request.action === 'scrape_list') {
-		data = scrape(request.data);
-		sendResponse({data: data});
-	}
+    if (request.action === "download") {
+        download(request.data);
+    } else if (request.action === "make_list") {
+        data = scrape(request.data);
+        download(data);
+        sendResponse({data: data});
+    } else if (request.action === 'next_page') {
+        window.location.href = $('#right .pagination a:last').attr('href');
+    } else if (request.action === 'scrape_list') {
+        data = scrape(request.data);
+        sendResponse({data: data});
+    }
 });
 
 function scrape(options)
 {
-	options = options || {};
+    options = options || {};
 
-	var $table = $('#main table.results')
-		, data = {first: [], last: [], email: [], position: [], company: [], other: [], tickettype: [], status: []}
-		, tmpDeck
-		, rowCount = 0
-		, statusesLength = 0
-		, ticketTypesLength = 0
-		, r
-		;
+    var $table = $('#main table.results')
+        , data = {first: [], last: [], email: [], position: [], company: [], other: [], tickettype: [], status: []}
+        , tmpDeck
+        , rowCount = 0
+        , statusesLength = 0
+        , ticketTypesLength = 0
+        , r
+        ;
 
-	if (options.ticketTypes && typeof options.ticketTypes === 'string') {
-		options.ticketTypes = options.ticketTypes.split(',').map(function (elem) {
-			return elem.trim();
-		});
-	}
+    if (options.ticketTypes && typeof options.ticketTypes === 'string') {
+        options.ticketTypes = options.ticketTypes.split(',').map(function (elem) {
+            return elem.trim();
+        });
+    }
 
-	if (options.statuses && typeof options.statuses === 'string') {
-		options.statuses = options.statuses.split(',').map(function (elem) {
-			return elem.trim();
-		});
-	}
+    if (options.statuses && typeof options.statuses === 'string') {
+        options.statuses = options.statuses.split(',').map(function (elem) {
+            return elem.trim();
+        });
+    }
 
-	options.emailTemplate = options.emailTemplate || "";
+    options.emailTemplate = options.emailTemplate || "";
 
-	if (options.ticketTypes && options.ticketTypes.length) {
-		ticketTypesLength = options.ticketTypes.length;
-	}
+    if (options.ticketTypes && options.ticketTypes.length) {
+        ticketTypesLength = options.ticketTypes.length;
+    }
 
-	if (options.statuses && options.statuses.length) {
-		statusesLength = options.statuses.length;
-	}
+    if (options.statuses && options.statuses.length) {
+        statusesLength = options.statuses.length;
+    }
 
-	// gather data
-	$table.find('td.name').each(function (i, element) {
-		var name = $(element).find('> a').text().trim()
-			, parts = name.split(' ')
-			, description = $(element).find('.description').text().split(',')
-			, position = description[0] ? String(description[0]).trim() : ''
-			, company = description[1] ? String(description[1]).trim(): ''
-			, last = parts.pop().trim()
-			, first = parts.join(' ').trim()
-			, emailName = []
-			, nameHref = $(element).find('> a').attr('href').split('/')
-			, other = nameHref[2] ? String(nameHref[2]).trim() : ''
-			;
+    // gather data
+    $table.find('td.name').each(function (i, element) {
+        var name = $(element).find('> a').text().trim()
+            , parts = name.split(' ')
+            , description = $(element).find('.description').text().split(',')
+            , position = description[0] ? String(description[0]).trim() : ''
+            , company = description[1] ? String(description[1]).trim(): ''
+            , last = parts.pop().trim()
+            , first = parts.join(' ').trim()
+            , emailName = []
+            , nameHref = $(element).find('> a').attr('href').split('/')
+            , other = nameHref[2] ? String(nameHref[2]).trim() : ''
+            ;
 
-		data.first.push(first);
-		data.last.push(last);
+        data.first.push(first);
+        data.last.push(last);
 
-		if (first.length) {
-			emailName.push(first);
-		}
+        if (first.length) {
+            emailName.push(first);
+        }
 
-		if (last.length) {
-			emailName.push(last);
-		}
+        if (last.length) {
+            emailName.push(last);
+        }
 
-		if (options.emailTemplate) {
-			data.email.push(options.emailTemplate.replace('{$name}', emailName.join(' ').split(' ').map(prepareEmailName).join('.')));
-		}
+        if (options.emailTemplate) {
+            data.email.push(options.emailTemplate.replace('{$name}', emailName.join(' ').split(' ').map(prepareEmailName).join('.')));
+        }
 
-		data.other.push(other);
-		data.company.push(company);
-		data.position.push(position);
+        data.other.push(other);
+        data.company.push(company);
+        data.position.push(position);
 
-		if (ticketTypesLength) {
-			data.tickettype.push(options.ticketTypes[Math.floor(Math.random() * (ticketTypesLength))])
-		}
+        if (ticketTypesLength) {
+            data.tickettype.push(options.ticketTypes[Math.floor(Math.random() * (ticketTypesLength))])
+        }
 
-		if (statusesLength) {
-			data.status.push(options.statuses[Math.floor(Math.random() * (statusesLength))]);
-		}
+        if (statusesLength) {
+            data.status.push(options.statuses[Math.floor(Math.random() * (statusesLength))]);
+        }
 
-		rowCount++;
-	});
+        rowCount++;
+    });
 
-	// post process any data
-	if (options.shuffleCompany) {
-		shuffleArray(data.company);
-	}
+    // post process any data
+    if (options.shuffleCompany) {
+        shuffleArray(data.company);
+    }
 
-	if (options.shuffleFirst) {
-		// attempt to keep the position with the first name just to make the data make a bit more sense
-		tmpDeck = [];
+    if (options.shuffleFirst) {
+        // attempt to keep the position with the first name just to make the data make a bit more sense
+        tmpDeck = [];
 
-		for (r = 0; r < rowCount; r++) {
-			tmpDeck.push({
-				first: data.first[r],
-				position: data.position[r]
-			});
-		}
+        for (r = 0; r < rowCount; r++) {
+            tmpDeck.push({
+                first: data.first[r],
+                position: data.position[r]
+            });
+        }
 
-		shuffleArray(tmpDeck);
+        shuffleArray(tmpDeck);
 
-		for (r = 0; r < rowCount; r++) {
-			data.first[r] = tmpDeck[r].first;
-			data.position[r] = tmpDeck[r].position;
-		}
-	}
+        for (r = 0; r < rowCount; r++) {
+            data.first[r] = tmpDeck[r].first;
+            data.position[r] = tmpDeck[r].position;
+        }
+    }
 
-	data.emailTemplate = options.emailTemplate;
-	data.length = rowCount;
-	data.ticketTypesLength = ticketTypesLength;
-	data.statusesLength = statusesLength;
-	data.start = $table.find('tr.detailed:first .number').text();
-	data.end = $table.find('tr.detailed:last .number').text();
+    data.emailTemplate = options.emailTemplate;
+    data.length = rowCount;
+    data.ticketTypesLength = ticketTypesLength;
+    data.statusesLength = statusesLength;
+    data.start = $table.find('tr.detailed:first .number').text();
+    data.end = $table.find('tr.detailed:last .number').text();
 
-	return data;
+    return data;
 }
 
 function download(data)
 {
-	var csv = "data:text/csv;charset=utf-8,"
-		, length = data.length
-		, line
-		, lines = [['First', 'Last', 'Email', 'Position', 'Company', 'Other']]
-		, link = document.createElement('a')
-		, r
-		, urlParameters = {}
-		;
+    var csv = "data:text/csv;charset=utf-8,"
+        , length = data.length
+        , line
+        , lines = [['First', 'Last', 'Email', 'Position', 'Company', 'Other']]
+        , link = document.createElement('a')
+        , r
+        , urlParameters = {}
+        ;
 
-	if (!data.emailTemplate) {
-		lines[0].splice(2, 1);
-	}
+    if (!data.emailTemplate) {
+        lines[0].splice(2, 1);
+    }
 
-	if (data.ticketTypesLength) {
-		lines[0].push('Ticket Type');
-	}
+    if (data.ticketTypesLength) {
+        lines[0].push('Ticket Type');
+    }
 
-	if (data.statusesLength) {
-		lines[0].push('Status');
-	}
+    if (data.statusesLength) {
+        lines[0].push('Status');
+    }
 
-	// create lines
-	for (r = 0; r < length; r++) {
-		line = [
-			data.first[r],
-			data.last[r],
-			data.position[r],
-			data.company[r],
-			data.other[r]
-		];
+    // create lines
+    for (r = 0; r < length; r++) {
+        line = [
+            data.first[r],
+            data.last[r],
+            data.position[r],
+            data.company[r],
+            data.other[r]
+        ];
 
-		if (data.emailTemplate) {
-			line.splice(2, 0, data.email[r]);
-		}
+        if (data.emailTemplate) {
+            line.splice(2, 0, data.email[r]);
+        }
 
-		if (data.ticketTypesLength) {
-			line.push(data.tickettype[r]);
-		}
+        if (data.ticketTypesLength) {
+            line.push(data.tickettype[r]);
+        }
 
-		if (data.statusesLength) {
-			line.push(data.status[r]);
-		}
+        if (data.statusesLength) {
+            line.push(data.status[r]);
+        }
 
-		lines.push(line.join(','));
-	}
+        lines.push(line.join(','));
+    }
 
-	window.location.href.split('?').pop().split('&').forEach(function (element) {
-		var pieces = element.split('=');
+    window.location.href.split('?').pop().split('&').forEach(function (element) {
+        var pieces = element.split('=');
 
-		urlParameters[pieces[0]] = pieces[1];
-	});
+        urlParameters[pieces[0]] = pieces[1];
+    });
 
-	csv += lines.join("\n");
-	link.setAttribute('href', encodeURI(csv));
-	link.setAttribute(
-		'download',
-		'IMDb-' + (urlParameters['birth_monthday'] ? urlParameters['birth_monthday'] : urlParameters['birth_month'] + '-' + urlParameters['birth_day'])
-		+ ((data.ticketTypesLength || data.statusesLength) ? '-guest-list' : '-group') + '-'
-		+ parseInt(data.start) + '_' + parseInt(data.end)
-		+ '.csv'
-	);
-	link.click();
+    csv += lines.join("\n");
+    link.setAttribute('href', encodeURI(csv));
+    link.setAttribute(
+        'download',
+        'IMDb-' + (urlParameters['birth_monthday'] ? urlParameters['birth_monthday'] : urlParameters['birth_month'] + '-' + urlParameters['birth_day'])
+        + ((data.ticketTypesLength || data.statusesLength) ? '-guest-list' : '-group') + '-'
+        + parseInt(data.start) + '_' + parseInt(data.end)
+        + '.csv'
+    );
+    link.click();
 }
 
 // force lower case and get rid of any dots at the start or end of the string
 function prepareEmailName(name)
 {
-	name = name.toLowerCase();
+    name = name.toLowerCase();
 
-	if (name.substr(0, 1) === '.') {
-		name = name.substr(1);
-	}
+    if (name.substr(0, 1) === '.') {
+        name = name.substr(1);
+    }
 
-	if (name.substr(-1) === '.') {
-		name = name.substr(0, name.length - 1);
-	}
+    if (name.substr(-1) === '.') {
+        name = name.substr(0, name.length - 1);
+    }
 
-	// replace using same rules from FuzzySearch
-	return name.replace(/[\t\n\r`~!@#$%^&*()+=;:'"<>?,/\\|]/g, '');
+    // replace using same rules from FuzzySearch
+    return name.replace(/[\t\n\r`~!@#$%^&*()+=;:'"<>?,/\\|]/g, '');
 }
 
 // http://bost.ocks.org/mike/shuffle/
 function shuffleArray(array)
 {
-	var m = array.length, t, i;
+    var m = array.length, t, i;
 
-	// While there remain elements to shuffle...
-	while (m) {
-		// Pick a remaining element…
-		i = Math.floor(Math.random() * m--);
+    // While there remain elements to shuffle...
+    while (m) {
+        // Pick a remaining element…
+        i = Math.floor(Math.random() * m--);
 
-		// And swap it with the current element.
-		t = array[m];
-		array[m] = array[i];
-		array[i] = t;
-	}
+        // And swap it with the current element.
+        t = array[m];
+        array[m] = array[i];
+        array[i] = t;
+    }
 
-	return array;
+    return array;
 }
--- js/popup.js
+++ js/popup.js
@@ -1,182 +1,182 @@
 (function () {
-	'use strict';
+    'use strict';
 
-	angular.module('app.ListMaker', []).controller('ListMakerCtrl', [
-		'$q', '$scope',
-		function ($q, $scope) {
-			var vm = this;
+    angular.module('app.ListMaker', []).controller('ListMakerCtrl', [
+        '$q', '$scope',
+        function ($q, $scope) {
+            var vm = this;
 
-			angular.extend(vm, {
-				make: make,
-				openBirthdayLink: openBirthdayLink,
-				settings: {},
-				scrape: scrape,
-				scrapeAndDownload: scrapeAndDownload,
-				scrapeAndNextPage: scrapeAndNextPage,
-				submit: angular.noop,
-				form: {}
-			});
+            angular.extend(vm, {
+                make: make,
+                openBirthdayLink: openBirthdayLink,
+                settings: {},
+                scrape: scrape,
+                scrapeAndDownload: scrapeAndDownload,
+                scrapeAndNextPage: scrapeAndNextPage,
+                submit: angular.noop,
+                form: {}
+            });
 
-			initialize();
+            initialize();
 
-			//////////////////////////////////////////// controller methods ////////////////////////////////////////////
+            //////////////////////////////////////////// controller methods ////////////////////////////////////////////
 
-			/**
-			 * Sends the event to the content page to download the list with the given data
-			 * @param {object} data
-			 */
-			function download(data)
-			{
-				chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
-					chrome.tabs.sendMessage(tabs[0].id, {
-						action: "download",
-						data: data
-					});
-				});
-			}
+            /**
+             * Sends the event to the content page to download the list with the given data
+             * @param {object} data
+             */
+            function download(data)
+            {
+                chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
+                    chrome.tabs.sendMessage(tabs[0].id, {
+                        action: "download",
+                        data: data
+                    });
+                });
+            }
 
-			/**
-			 * Initializes the controller
-			 */
-			function initialize()
-			{
-				chrome.storage.local.get(null, function (options) {
-					options = options || {};
+            /**
+             * Initializes the controller
+             */
+            function initialize()
+            {
+                chrome.storage.local.get(null, function (options) {
+                    options = options || {};
 
-					angular.forEach(options.settings, function (setting, key) {
-						vm.settings[key] = setting;
-					});
+                    angular.forEach(options.settings, function (setting, key) {
+                        vm.settings[key] = setting;
+                    });
 
-					$scope.$applyAsync();
-				});
-			}
+                    $scope.$applyAsync();
+                });
+            }
 
-			/**
-			 * Sends the event to the content page to parse the current IMDb birthday page and downloads the list
-			 * @returns {Promise}
-			 */
-			function make()
-			{
-				var deferred = $q.defer();
+            /**
+             * Sends the event to the content page to parse the current IMDb birthday page and downloads the list
+             * @returns {Promise}
+             */
+            function make()
+            {
+                var deferred = $q.defer();
 
-				chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
-					chrome.tabs.sendMessage(tabs[0].id, {
-						action: "make_list",
-						deferred: deferred,
-						data: vm.settings
-					});
-				});
+                chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
+                    chrome.tabs.sendMessage(tabs[0].id, {
+                        action: "make_list",
+                        deferred: deferred,
+                        data: vm.settings
+                    });
+                });
 
-				chrome.storage.local.set({settings: vm.settings});
+                chrome.storage.local.set({settings: vm.settings});
 
-				return deferred.promise;
-			}
+                return deferred.promise;
+            }
 
-			/**
-			 * Sends the event to content page to go to the next page
-			 */
-			function nextPage()
-			{
-				chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
-					chrome.tabs.sendMessage(tabs[0].id, {
-						action: "next_page"
-					});
-				});
-			}
+            /**
+             * Sends the event to content page to go to the next page
+             */
+            function nextPage()
+            {
+                chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
+                    chrome.tabs.sendMessage(tabs[0].id, {
+                        action: "next_page"
+                    });
+                });
+            }
 
-			function openBirthdayLink()
-			{
-				var date = new Date();
+            function openBirthdayLink()
+            {
+                var date = new Date();
 
-				chrome.tabs.create({url: "http://www.imdb.com/search/name?refine=birth_monthday&birth_monthday=" + (date.getMonth() + 1) + '-' + date.getDate()});
-			}
+                chrome.tabs.create({url: "http://www.imdb.com/search/name?refine=birth_monthday&birth_monthday=" + (date.getMonth() + 1) + '-' + date.getDate()});
+            }
 
-			/**
-			 * Sends the event to the content page to parse the current IMDb birthday page
-			 */
-			function scrape()
-			{
-				var deferred = $q.defer();
+            /**
+             * Sends the event to the content page to parse the current IMDb birthday page
+             */
+            function scrape()
+            {
+                var deferred = $q.defer();
 
-				chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
-					chrome.tabs.sendMessage(tabs[0].id, {
-						action: "scrape_list",
-						data: vm.settings
-					}, function (response) {
-						deferred.resolve(response.data || {});
-					});
-				});
+                chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
+                    chrome.tabs.sendMessage(tabs[0].id, {
+                        action: "scrape_list",
+                        data: vm.settings
+                    }, function (response) {
+                        deferred.resolve(response.data || {});
+                    });
+                });
 
-				chrome.storage.local.set({settings: vm.settings});
+                chrome.storage.local.set({settings: vm.settings});
 
-				return deferred.promise.then(function (data) {
-					var deferred = $q.defer();
+                return deferred.promise.then(function (data) {
+                    var deferred = $q.defer();
 
-					if (!angular.isObject(data)) {
-						return $q.reject({status: 400, statusText: "Invalid data"});
-					}
+                    if (!angular.isObject(data)) {
+                        return $q.reject({status: 400, statusText: "Invalid data"});
+                    }
 
-					// get current
-					chrome.storage.local.get(null, function (options) {
-						options = options || {};
+                    // get current
+                    chrome.storage.local.get(null, function (options) {
+                        options = options || {};
 
-						if (!angular.isObject(options.data)) {
-							options.data = {};
-						}
+                        if (!angular.isObject(options.data)) {
+                            options.data = {};
+                        }
 
-						if (angular.equals({}, options.data)) { // no previous data
-							options.data = data;
-						} else { // append new data
-							['company', 'email', 'first', 'last', 'other', 'position', 'status', 'tickettype'].forEach(function (key) {
-								if (!angular.isArray(options.data[key])) {
-									options.data[key] = [];
-								}
+                        if (angular.equals({}, options.data)) { // no previous data
+                            options.data = data;
+                        } else { // append new data
+                            ['company', 'email', 'first', 'last', 'other', 'position', 'status', 'tickettype'].forEach(function (key) {
+                                if (!angular.isArray(options.data[key])) {
+                                    options.data[key] = [];
+                                }
 
-								options.data[key] = options.data[key].concat(data[key]);
-							});
+                                options.data[key] = options.data[key].concat(data[key]);
+                            });
 
-							options.data.length += data.length;
-						}
+                            options.data.length += data.length;
+                        }
 
-						// update ending range
-						options.data.end = data.end;
+                        // update ending range
+                        options.data.end = data.end;
 
-						chrome.storage.local.set({data: options.data}, function () {
-							deferred.resolve(options.data);
-						});
-					});
+                        chrome.storage.local.set({data: options.data}, function () {
+                            deferred.resolve(options.data);
+                        });
+                    });
 
-					return deferred.promise;
-				});
-			}
+                    return deferred.promise;
+                });
+            }
 
-			function scrapeAndDownload()
-			{
-				var deferred = $q.defer();
+            function scrapeAndDownload()
+            {
+                var deferred = $q.defer();
 
-				return scrape().then(function (data) {
-					download(data);
+                return scrape().then(function (data) {
+                    download(data);
 
-					chrome.storage.local.set({data: {}}, function () {
-						deferred.resolve(data);
-					});
+                    chrome.storage.local.set({data: {}}, function () {
+                        deferred.resolve(data);
+                    });
 
-					return deferred.promise;
-				});
-			}
+                    return deferred.promise;
+                });
+            }
 
-			function scrapeAndNextPage()
-			{
-				return scrape().then(function (data) {
-					nextPage();
+            function scrapeAndNextPage()
+            {
+                return scrape().then(function (data) {
+                    nextPage();
 
-					return data;
-				});
-			}
-		}
-	]);
+                    return data;
+                });
+            }
+        }
+    ]);
 
-	angular.element(document).ready(function () {
-		angular.bootstrap(document, ['app.ListMaker']);
-	});
+    angular.element(document).ready(function () {
+        angular.bootstrap(document, ['app.ListMaker']);
+    });
 })();
